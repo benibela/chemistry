d4e3fa4dc3fc56257fbaa4e24c0f2ba45e030a67
benibela <benito@benibela.de>
2014-10-10 23:08:48 +0200

    update mirrors

--- manage.sh
+++ manage.sh
@@ -2,7 +2,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../../manageUtils.sh
 
-githubProject chemistry
+mirroredProject chemistry
 
 BASE=$HGROOT/programs/data/chemistry
 
